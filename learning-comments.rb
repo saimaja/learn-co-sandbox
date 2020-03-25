a968b4e9ec7b2a9b7a9a8b6f703abb2213eae3c1
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-25 17:56:56 +0000

    Done.

---
+++ learning-comments.rb
@@ -0,0 +1,23 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+#end
+#puts "Code after if...end"
+
+#chance_of_rain = 1
+#puts "Let's go outside!"
+#if chance_of_rain > 0.5
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always wear sunscreen!"
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
